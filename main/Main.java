eeac311251408c7637ce4a9a3014a3658ce97d5e
rambon99 <infinity-a-ward@hotmail.com>
2024-04-04 13:09:39 +0200

    Version 0.5 Update

--- main/Main.java
+++ main/Main.java
@@ -3,6 +3,12 @@ package main;
 import java.io.*;
 import java.util.*;
 import javax.xml.parsers.*;
+
+import main.Output.DebugBuilder;
+import main.Output.XMLBuilder;
+import main.Program.GUIBools;
+import main.Structure.XMLReader;
+import main.Swapping.*;
 import org.w3c.dom.*;
 import org.xml.sax.*;
 import java.util.zip.GZIPInputStream;
@@ -14,12 +20,9 @@ import main.Structure.ACharacter;
 import main.Structure.CharacterStarter;
 import main.Structure.ClassStarter;
 import main.Structure.AClasses;
-import main.Swapping.ClassSwapper;
-import main.Swapping.ItemSwapper;
 import main.Structure.AChapter;
 import main.Structure.ChapterStarter;
 import main.Randomizer.CharacterShuffler;
-import main.Swapping.CharSwapper;
 import main.Output.TextFile;
 import main.Program.GUI;
 import main.Program.FileChecker;
@@ -30,9 +33,9 @@ public class Main{
 		
 	}
 	
-	public void randomizeShit(boolean clb, boolean chb) throws Exception{
-		System.out.println(clb);
-		System.out.println(chb);
+	public void randomizeShit(GUIBools randomizedOptions, File folder, File xmlFile) throws Exception{
+		DebugBuilder.DebugOutput("Class Swap: " + randomizedOptions.characters);
+		DebugBuilder.DebugOutput("Character Swap: " + randomizedOptions.characters);
 		ArrayList<ACharacter> c = new ArrayList<ACharacter>();
 		ArrayList<AClasses> cl = new ArrayList<AClasses>();
 		ArrayList<AChapter> ch = new ArrayList<AChapter>();
@@ -41,33 +44,61 @@ public class Main{
 		CharacterStarter chars = new CharacterStarter();
 		ItemSwapper is = new ItemSwapper();
 		BinFiles bin = new BinFiles();
+		bin.SetFolder(folder);
 		ChapterStarter chaps = new ChapterStarter();
 		CharSwapper chs = new CharSwapper();
 		TextFile tf = new TextFile();
+		SkillSwapper skill = new SkillSwapper();
+		TextSwapper ts = new TextSwapper();
+		XMLBuilder xml = new XMLBuilder();
 		
 		StringBuilder statc = new StringBuilder();
 		bin.getStatic(statc);
 		
-		chars.setCharacter(c);
-		cls.setClasses(cl);
+		chars.setCharacter(c, statc);
+		cls.setClasses(cl, statc);
 		chaps.setChapters(ch);
 		
-		if (chb){
-			chs.swapChars(c, statc, ch);
-			if (!clb){
-				cs.adjustClasses(c, cl, statc);
+		if (randomizedOptions.characters || randomizedOptions.xml){
+			if (randomizedOptions.xml){
+				XMLReader xmlReader = new XMLReader();
+				c = xmlReader.ReturnXMLCharList(c, randomizedOptions, xmlFile);
 			}
+			else{
+				CharacterShuffler chash = new CharacterShuffler();
+				chash.randomizeCharacters(c);
+			}
+			//change story bool to fit whichever option is picked
+			chs.swapChars(c, statc, ch, randomizedOptions.cutscenes);
+			DebugBuilder.DebugOutput("Chars Swapped");
 		}
-		System.out.println("Chars Swapped");
-		if (clb){
+		if (randomizedOptions.classes || randomizedOptions.xml){
+			if (randomizedOptions.classes) {
+				ClassShuffler ran = new ClassShuffler();
+				ran.RandomizeClasses(c);
+			}
 			cs.swapClasses(c, cl, statc);
+			DebugBuilder.DebugOutput("Classes Swapped");
 		}
-		System.out.println("Classes Swapped");
+		else {
+			cs.adjustClasses(c, cl, statc);
+			DebugBuilder.DebugOutput("Classes Adjusted");
+		}
 		is.swapItems(c, cl, statc, ch);
-		System.out.println("Items Swapped");
-		
-		tf.makeTextFile(c);
-		System.out.println("End main");
+		DebugBuilder.DebugOutput("Items Swapped");
+		skill.SwapSkills(c, cl, statc, randomizedOptions.xml && !randomizedOptions.classes);
+		DebugBuilder.DebugOutput("Skills Swapped");
+		ts.SwapTexts(c, randomizedOptions.cutscenes, randomizedOptions.supports);
+		DebugBuilder.DebugOutput("texts swapped");
+
+		if (randomizedOptions.newXml){
+			xml.CreateXML(c, randomizedOptions);
+			DebugBuilder.DebugOutput("xml created");
+		}
+		if (randomizedOptions.textFile) {
+			tf.makeTextFile(c);
+		}
+		DebugBuilder.DebugOutput("End main");
 	}
 	
 }
